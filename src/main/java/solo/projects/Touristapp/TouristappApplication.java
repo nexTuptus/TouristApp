89c086544bb0585dbf07caa33a16359205acd23f
nexTuptus <bartekmoskala@gmail.com>
2020-11-22 09:55:43 +0100

    Initial commit

---
+++ src/main/java/solo/projects/Touristapp/TouristappApplication.java
@@ -0,0 +1,13 @@
+package solo.projects.Touristapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristappApplication.class, args);
+	}
+
+}
